8a0820b50317589f1587ea7240cd6a69cc6f5799
rachelmoore44 <rm493222@ohio.edu>
2024-01-23 20:10:30 -0500

    Hello

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Rachel Moore
+ *        Date: 1/23/24
+ * Description: C++ program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout <<"Hello Git!!"<< endl;
+    return 0;
+}// main
